edb110b956b602a78dd53223ea4def157abaeced
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2024-02-17 17:45:02 -0600

    implement kelvin to fahrenheit function

--- main.js
+++ main.js
@@ -2,5 +2,15 @@
 // Kelvin to Fahrenheit Conversion Application
 ///////////////////////////////////////////
 
+const KelvinToFahrenheit = (kelvin) => {
+    return (kelvin - 273.15) * 9/5 + 32;
+}
+
 console.log("//////////////////////////////");
 console.log("Kelvin to Fahrenheit Application\n");
+
+console.log("Fahreinheit for 273 kelvin is:");
+console.log(KelvinToFahrenheit(273));
+
+console.log("Fahreinheit for 280.34 kelvin is:");
+console.log(KelvinToFahrenheit(280.34));
